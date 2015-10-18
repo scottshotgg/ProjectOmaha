c5f26aaeac38036532bb37310a584ff762540200
scottshotgg <scgaydos@gmail.com>
2015-10-18 13:59:19 -0500

    Adding hardware stuff

---
+++ hardware/Project1/CPU1_FLASH/Source/F2837xS_SysCtrl.pp
@@ -0,0 +1,115 @@
+# FIXED
+
+Source/F2837xS_SysCtrl.obj: ../Source/F2837xS_SysCtrl.c
+Source/F2837xS_SysCtrl.obj: C:/Project1/include/F2837xS_device.h
+Source/F2837xS_SysCtrl.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.2/include/assert.h
+Source/F2837xS_SysCtrl.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.2/include/linkage.h
+Source/F2837xS_SysCtrl.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.2/include/stdarg.h
+Source/F2837xS_SysCtrl.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.2/include/stdbool.h
+Source/F2837xS_SysCtrl.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.2/include/yvals.h
+Source/F2837xS_SysCtrl.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.2/include/stdarg.h
+Source/F2837xS_SysCtrl.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.2/include/_lock.h
+Source/F2837xS_SysCtrl.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.2/include/stddef.h
+Source/F2837xS_SysCtrl.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.2/include/stdint.h
+Source/F2837xS_SysCtrl.obj: C:/Project1/include/F2837xS_adc.h
+Source/F2837xS_SysCtrl.obj: C:/Project1/include/F2837xS_analogsubsys.h
+Source/F2837xS_SysCtrl.obj: C:/Project1/include/F2837xS_cla.h
+Source/F2837xS_SysCtrl.obj: C:/Project1/include/F2837xS_cmpss.h
+Source/F2837xS_SysCtrl.obj: C:/Project1/include/F2837xS_cputimer.h
+Source/F2837xS_SysCtrl.obj: C:/Project1/include/F2837xS_dac.h
+Source/F2837xS_SysCtrl.obj: C:/Project1/include/F2837xS_dcsm.h
+Source/F2837xS_SysCtrl.obj: C:/Project1/include/F2837xS_dma.h
+Source/F2837xS_SysCtrl.obj: C:/Project1/include/F2837xS_defaultisr.h
+Source/F2837xS_SysCtrl.obj: C:/Project1/include/F2837xS_ecap.h
+Source/F2837xS_SysCtrl.obj: C:/Project1/include/F2837xS_emif.h
+Source/F2837xS_SysCtrl.obj: C:/Project1/include/F2837xS_epwm.h
+Source/F2837xS_SysCtrl.obj: C:/Project1/include/F2837xS_epwm_xbar.h
+Source/F2837xS_SysCtrl.obj: C:/Project1/include/F2837xS_eqep.h
+Source/F2837xS_SysCtrl.obj: C:/Project1/include/F2837xS_flash.h
+Source/F2837xS_SysCtrl.obj: C:/Project1/include/F2837xS_gpio.h
+Source/F2837xS_SysCtrl.obj: C:/Project1/include/F2837xS_i2c.h
+Source/F2837xS_SysCtrl.obj: C:/Project1/include/F2837xS_input_xbar.h
+Source/F2837xS_SysCtrl.obj: C:/Project1/include/F2837xS_mcbsp.h
+Source/F2837xS_SysCtrl.obj: C:/Project1/include/F2837xS_memconfig.h
+Source/F2837xS_SysCtrl.obj: C:/Project1/include/F2837xS_nmiintrupt.h
+Source/F2837xS_SysCtrl.obj: C:/Project1/include/F2837xS_output_xbar.h
+Source/F2837xS_SysCtrl.obj: C:/Project1/include/F2837xS_piectrl.h
+Source/F2837xS_SysCtrl.obj: C:/Project1/include/F2837xS_pievect.h
+Source/F2837xS_SysCtrl.obj: C:/Project1/include/F2837xS_sci.h
+Source/F2837xS_SysCtrl.obj: C:/Project1/include/F2837xS_sdfm.h
+Source/F2837xS_SysCtrl.obj: C:/Project1/include/F2837xS_spi.h
+Source/F2837xS_SysCtrl.obj: C:/Project1/include/F2837xS_sysctrl.h
+Source/F2837xS_SysCtrl.obj: C:/Project1/include/F2837xS_trig_xbar.h
+Source/F2837xS_SysCtrl.obj: C:/Project1/include/F2837xS_upp.h
+Source/F2837xS_SysCtrl.obj: C:/Project1/include/F2837xS_xbar.h
+Source/F2837xS_SysCtrl.obj: C:/Project1/include/F2837xS_xint.h
+Source/F2837xS_SysCtrl.obj: C:/Project1/include/F2837xS_Examples.h
+Source/F2837xS_SysCtrl.obj: C:/Project1/include/F2837xS_GlobalPrototypes.h
+Source/F2837xS_SysCtrl.obj: C:/Project1/include/F2837xS_cputimervars.h
+Source/F2837xS_SysCtrl.obj: C:/Project1/include/F2837xS_Cla_defines.h
+Source/F2837xS_SysCtrl.obj: C:/Project1/include/F2837xS_EPwm_defines.h
+Source/F2837xS_SysCtrl.obj: C:/Project1/include/F2837xS_Adc_defines.h
+Source/F2837xS_SysCtrl.obj: C:/Project1/include/F2837xS_Emif_defines.h
+Source/F2837xS_SysCtrl.obj: C:/Project1/include/F2837xS_Gpio_defines.h
+Source/F2837xS_SysCtrl.obj: C:/Project1/include/F2837xS_I2c_defines.h
+Source/F2837xS_SysCtrl.obj: C:/Project1/include/F2837xS_Pie_defines.h
+Source/F2837xS_SysCtrl.obj: C:/Project1/include/F2837xS_Dma_defines.h
+Source/F2837xS_SysCtrl.obj: C:/Project1/include/F2837xS_SysCtrl_defines.h
+Source/F2837xS_SysCtrl.obj: C:/Project1/include/F2837xS_Upp_defines.h
+
+../Source/F2837xS_SysCtrl.c: 
+C:/Project1/include/F2837xS_device.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.2/include/assert.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.2/include/linkage.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.2/include/stdarg.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.2/include/stdbool.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.2/include/yvals.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.2/include/stdarg.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.2/include/_lock.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.2/include/stddef.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.2/include/stdint.h: 
+C:/Project1/include/F2837xS_adc.h: 
+C:/Project1/include/F2837xS_analogsubsys.h: 
+C:/Project1/include/F2837xS_cla.h: 
+C:/Project1/include/F2837xS_cmpss.h: 
+C:/Project1/include/F2837xS_cputimer.h: 
+C:/Project1/include/F2837xS_dac.h: 
+C:/Project1/include/F2837xS_dcsm.h: 
+C:/Project1/include/F2837xS_dma.h: 
+C:/Project1/include/F2837xS_defaultisr.h: 
+C:/Project1/include/F2837xS_ecap.h: 
+C:/Project1/include/F2837xS_emif.h: 
+C:/Project1/include/F2837xS_epwm.h: 
+C:/Project1/include/F2837xS_epwm_xbar.h: 
+C:/Project1/include/F2837xS_eqep.h: 
+C:/Project1/include/F2837xS_flash.h: 
+C:/Project1/include/F2837xS_gpio.h: 
+C:/Project1/include/F2837xS_i2c.h: 
+C:/Project1/include/F2837xS_input_xbar.h: 
+C:/Project1/include/F2837xS_mcbsp.h: 
+C:/Project1/include/F2837xS_memconfig.h: 
+C:/Project1/include/F2837xS_nmiintrupt.h: 
+C:/Project1/include/F2837xS_output_xbar.h: 
+C:/Project1/include/F2837xS_piectrl.h: 
+C:/Project1/include/F2837xS_pievect.h: 
+C:/Project1/include/F2837xS_sci.h: 
+C:/Project1/include/F2837xS_sdfm.h: 
+C:/Project1/include/F2837xS_spi.h: 
+C:/Project1/include/F2837xS_sysctrl.h: 
+C:/Project1/include/F2837xS_trig_xbar.h: 
+C:/Project1/include/F2837xS_upp.h: 
+C:/Project1/include/F2837xS_xbar.h: 
+C:/Project1/include/F2837xS_xint.h: 
+C:/Project1/include/F2837xS_Examples.h: 
+C:/Project1/include/F2837xS_GlobalPrototypes.h: 
+C:/Project1/include/F2837xS_cputimervars.h: 
+C:/Project1/include/F2837xS_Cla_defines.h: 
+C:/Project1/include/F2837xS_EPwm_defines.h: 
+C:/Project1/include/F2837xS_Adc_defines.h: 
+C:/Project1/include/F2837xS_Emif_defines.h: 
+C:/Project1/include/F2837xS_Gpio_defines.h: 
+C:/Project1/include/F2837xS_I2c_defines.h: 
+C:/Project1/include/F2837xS_Pie_defines.h: 
+C:/Project1/include/F2837xS_Dma_defines.h: 
+C:/Project1/include/F2837xS_SysCtrl_defines.h: 
+C:/Project1/include/F2837xS_Upp_defines.h: 
---
+++ hardware/Project1/CPU1_FLASH/Source/subdir_vars.mk
@@ -0,0 +1,176 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+ASM_SRCS += \
+../Source/F2837xS_CodeStartBranch.asm \
+../Source/F2837xS_DBGIER.asm \
+../Source/F2837xS_usDelay.asm \
+../Source/FIR_f32.asm 
+
+C_SRCS += \
+../Source/F2837xS_Adc.c \
+../Source/F2837xS_CpuTimers.c \
+../Source/F2837xS_DefaultISR.c \
+../Source/F2837xS_Dma.c \
+../Source/F2837xS_EPwm.c \
+../Source/F2837xS_EQep.c \
+../Source/F2837xS_Emif.c \
+../Source/F2837xS_GlobalVariableDefs.c \
+../Source/F2837xS_Gpio.c \
+../Source/F2837xS_I2C.c \
+../Source/F2837xS_Mcbsp.c \
+../Source/F2837xS_PieCtrl.c \
+../Source/F2837xS_PieVect.c \
+../Source/F2837xS_Sci.c \
+../Source/F2837xS_Spi.c \
+../Source/F2837xS_SysCtrl.c \
+../Source/F2837xS_Upp.c \
+../Source/F2837xS_sci_io.c \
+../Source/F2837xS_sdfm_drivers.c \
+../Source/F2837xS_struct.c \
+../Source/examples_setup.c 
+
+OBJS += \
+./Source/F2837xS_Adc.obj \
+./Source/F2837xS_CodeStartBranch.obj \
+./Source/F2837xS_CpuTimers.obj \
+./Source/F2837xS_DBGIER.obj \
+./Source/F2837xS_DefaultISR.obj \
+./Source/F2837xS_Dma.obj \
+./Source/F2837xS_EPwm.obj \
+./Source/F2837xS_EQep.obj \
+./Source/F2837xS_Emif.obj \
+./Source/F2837xS_GlobalVariableDefs.obj \
+./Source/F2837xS_Gpio.obj \
+./Source/F2837xS_I2C.obj \
+./Source/F2837xS_Mcbsp.obj \
+./Source/F2837xS_PieCtrl.obj \
+./Source/F2837xS_PieVect.obj \
+./Source/F2837xS_Sci.obj \
+./Source/F2837xS_Spi.obj \
+./Source/F2837xS_SysCtrl.obj \
+./Source/F2837xS_Upp.obj \
+./Source/F2837xS_sci_io.obj \
+./Source/F2837xS_sdfm_drivers.obj \
+./Source/F2837xS_struct.obj \
+./Source/F2837xS_usDelay.obj \
+./Source/FIR_f32.obj \
+./Source/examples_setup.obj 
+
+ASM_DEPS += \
+./Source/F2837xS_CodeStartBranch.pp \
+./Source/F2837xS_DBGIER.pp \
+./Source/F2837xS_usDelay.pp \
+./Source/FIR_f32.pp 
+
+C_DEPS += \
+./Source/F2837xS_Adc.pp \
+./Source/F2837xS_CpuTimers.pp \
+./Source/F2837xS_DefaultISR.pp \
+./Source/F2837xS_Dma.pp \
+./Source/F2837xS_EPwm.pp \
+./Source/F2837xS_EQep.pp \
+./Source/F2837xS_Emif.pp \
+./Source/F2837xS_GlobalVariableDefs.pp \
+./Source/F2837xS_Gpio.pp \
+./Source/F2837xS_I2C.pp \
+./Source/F2837xS_Mcbsp.pp \
+./Source/F2837xS_PieCtrl.pp \
+./Source/F2837xS_PieVect.pp \
+./Source/F2837xS_Sci.pp \
+./Source/F2837xS_Spi.pp \
+./Source/F2837xS_SysCtrl.pp \
+./Source/F2837xS_Upp.pp \
+./Source/F2837xS_sci_io.pp \
+./Source/F2837xS_sdfm_drivers.pp \
+./Source/F2837xS_struct.pp \
+./Source/examples_setup.pp 
+
+C_DEPS__QUOTED += \
+"Source\F2837xS_Adc.pp" \
+"Source\F2837xS_CpuTimers.pp" \
+"Source\F2837xS_DefaultISR.pp" \
+"Source\F2837xS_Dma.pp" \
+"Source\F2837xS_EPwm.pp" \
+"Source\F2837xS_EQep.pp" \
+"Source\F2837xS_Emif.pp" \
+"Source\F2837xS_GlobalVariableDefs.pp" \
+"Source\F2837xS_Gpio.pp" \
+"Source\F2837xS_I2C.pp" \
+"Source\F2837xS_Mcbsp.pp" \
+"Source\F2837xS_PieCtrl.pp" \
+"Source\F2837xS_PieVect.pp" \
+"Source\F2837xS_Sci.pp" \
+"Source\F2837xS_Spi.pp" \
+"Source\F2837xS_SysCtrl.pp" \
+"Source\F2837xS_Upp.pp" \
+"Source\F2837xS_sci_io.pp" \
+"Source\F2837xS_sdfm_drivers.pp" \
+"Source\F2837xS_struct.pp" \
+"Source\examples_setup.pp" 
+
+OBJS__QUOTED += \
+"Source\F2837xS_Adc.obj" \
+"Source\F2837xS_CodeStartBranch.obj" \
+"Source\F2837xS_CpuTimers.obj" \
+"Source\F2837xS_DBGIER.obj" \
+"Source\F2837xS_DefaultISR.obj" \
+"Source\F2837xS_Dma.obj" \
+"Source\F2837xS_EPwm.obj" \
+"Source\F2837xS_EQep.obj" \
+"Source\F2837xS_Emif.obj" \
+"Source\F2837xS_GlobalVariableDefs.obj" \
+"Source\F2837xS_Gpio.obj" \
+"Source\F2837xS_I2C.obj" \
+"Source\F2837xS_Mcbsp.obj" \
+"Source\F2837xS_PieCtrl.obj" \
+"Source\F2837xS_PieVect.obj" \
+"Source\F2837xS_Sci.obj" \
+"Source\F2837xS_Spi.obj" \
+"Source\F2837xS_SysCtrl.obj" \
+"Source\F2837xS_Upp.obj" \
+"Source\F2837xS_sci_io.obj" \
+"Source\F2837xS_sdfm_drivers.obj" \
+"Source\F2837xS_struct.obj" \
+"Source\F2837xS_usDelay.obj" \
+"Source\FIR_f32.obj" \
+"Source\examples_setup.obj" 
+
+ASM_DEPS__QUOTED += \
+"Source\F2837xS_CodeStartBranch.pp" \
+"Source\F2837xS_DBGIER.pp" \
+"Source\F2837xS_usDelay.pp" \
+"Source\FIR_f32.pp" 
+
+C_SRCS__QUOTED += \
+"../Source/F2837xS_Adc.c" \
+"../Source/F2837xS_CpuTimers.c" \
+"../Source/F2837xS_DefaultISR.c" \
+"../Source/F2837xS_Dma.c" \
+"../Source/F2837xS_EPwm.c" \
+"../Source/F2837xS_EQep.c" \
+"../Source/F2837xS_Emif.c" \
+"../Source/F2837xS_GlobalVariableDefs.c" \
+"../Source/F2837xS_Gpio.c" \
+"../Source/F2837xS_I2C.c" \
+"../Source/F2837xS_Mcbsp.c" \
+"../Source/F2837xS_PieCtrl.c" \
+"../Source/F2837xS_PieVect.c" \
+"../Source/F2837xS_Sci.c" \
+"../Source/F2837xS_Spi.c" \
+"../Source/F2837xS_SysCtrl.c" \
+"../Source/F2837xS_Upp.c" \
+"../Source/F2837xS_sci_io.c" \
+"../Source/F2837xS_sdfm_drivers.c" \
+"../Source/F2837xS_struct.c" \
+"../Source/examples_setup.c" 
+
+ASM_SRCS__QUOTED += \
+"../Source/F2837xS_CodeStartBranch.asm" \
+"../Source/F2837xS_DBGIER.asm" \
+"../Source/F2837xS_usDelay.asm" \
+"../Source/FIR_f32.asm" 
+
+
