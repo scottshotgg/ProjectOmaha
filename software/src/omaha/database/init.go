abb437238393b691d2e938ba6bca3e87a351bdbc
mbh621 <hall.max21@gmail.com>
2015-10-25 21:58:42 -0500

    added db init code

--- software/src/omaha/database/init.go
+++ software/src/omaha/database/init.go
@@ -2,17 +2,40 @@ package database
 
 import (
 	"database/sql"
-	"fmt"
 	_ "github.com/mattn/go-sqlite3"
+	"log"
 	"omaha/util"
 )
 
 func InitDB() {
-	_, err := sql.Open("sqlite3", util.GetOmahaPath()+"/omaha.db")
-	if err != nil {
-		fmt.Println("DB Initialization Failed")
-	} else {
-		fmt.Println("DB Initialized")
-	}
 
+	db, err := sql.Open("sqlite3", util.GetOmahaPath()+"/omaha.db")
+	defer db.Close()
+	if err != nil {
+		log.Fatalf("DB file doesn't exist\n")
+	}
+	// check if user table exists
+	var name string
+	err = db.QueryRow("SELECT name FROM sqlite_master WHERE type='table' AND name='account';").Scan(&name)
+	switch {
+	case err == sql.ErrNoRows:
+		// table doesn't exist
+		createAccountTable(db)
+	case err != nil:
+		log.Fatal(err)
+	}
+}
+
+func createAccountTable(db *sql.DB) {
+	_, err := db.Exec(`CREATE TABLE  account (
+		uid INTEGER PRIMARY KEY AUTOINCREMENT,
+		username VARCHAR(50) NOT NULL,
+		name VARCHAR(50),
+		password VARCHAR(20) NOT NULL
+		);`) // yes, pw shouldn't be plain text
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		log.Println("Created account table")
+	}
 }
