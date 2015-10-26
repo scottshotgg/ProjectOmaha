2f9d37b12f50a4aeb7a5b3847369404f249bf29d
mbh621 <hall.max21@gmail.com>
2015-10-26 16:52:22 -0500

    uncommented port write

--- software/src/omaha/system/serial.go
+++ software/src/omaha/system/serial.go
@@ -20,15 +20,15 @@ func HandleControllerMessages() {
 		req := <-MessageChan
 		status := GetSystemStatus()
 		if !status.IsDebug() {
-			//status.Port.Write(req.Data)			// I dont get what this is doing here....?
+			_, err := status.Port.Write(req.Data) // I dont get what this is doing here....?
 
-			//n, err := s.Write([]byte("test"))		// req.Data goes here?
-        	//if err != nil {
-            //	log.Fatal(err)
-        	//}
+			//n, err := s.Write([]byte("test")) // req.Data goes here?
+			if err != nil {
+				log.Fatal(err)
+			}
 		}
 		if req.OnWrite != nil {
-			result := req.OnWrite()					// I'm a bit confused on where the actual data is sent
+			result := req.OnWrite() // I'm a bit confused on where the actual data is sent
 			if req.ResultChan != nil {
 				req.ResultChan <- result
 			}
@@ -77,7 +77,7 @@ func (status *SystemStatus) ReadData(buffer []byte) bool {
 }
 
 func (status *SystemStatus) InitializePort() {
-/*	options := serial.OpenOptions{
+	/*	options := serial.OpenOptions{
 		PortName:        "/dev/ttyUSB0",
 		BaudRate:        9600,
 		DataBits:        8,
@@ -85,11 +85,11 @@ func (status *SystemStatus) InitializePort() {
 		MinimumReadSize: 1,
 	}*/
 
-	c := &serial.Config{Name: "/dev/ttyUSB0", Baud: 9600, ReadTimeout: time.Second}	// Probably want a half a second. This is plenty for the microcontroller to have time to respond
-	s, err := serial.OpenPort(c)															// If it hasn't responded by now then it isn't going to most likely
-    if err != nil {																			// Actually, you can't do this, for some reason it has to be an int value and cant be a floating value
-            log.Fatal(err)
-    }
+	c := &serial.Config{Name: "/dev/ttyUSB0", Baud: 9600, ReadTimeout: time.Second} // Probably want a half a second. This is plenty for the microcontroller to have time to respond
+	s, err := serial.OpenPort(c)                                                    // If it hasn't responded by now then it isn't going to most likely
+	if err != nil {                                                                 // Actually, you can't do this, for some reason it has to be an int value and cant be a floating value
+		log.Fatal(err)
+	}
 
 	fmt.Println("Hello " + c.Name)
 
