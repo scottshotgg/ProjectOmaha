fa8b505f3f73f24b671b60ec63482b0a67bc51cf
mbh621 <hall.max21@gmail.com>
2015-11-20 18:33:33 -0600

    Fixed the image processing script. The function definitions have to come before they are called.

--- software/src/omaha/main.py
+++ software/src/omaha/main.py
@@ -4,49 +4,6 @@ import math
 import json
 import argparse
 
-# parse input parameters
-parser = argparse.ArgumentParser(description="Get speaker coordinates from map")
-parser.add_argument('imagePath', metavar="I")
-parser.add_argument('outputPath', metavar="O", type=argparse.FileType('w'))
-parsedInput = vars(parser.parse_args())
-outputFile = parsedInput["outputPath"]
-inputFileName = parsedInput["imagePath"]
-
-orig = cv2.imread(inputFileName)
-origCopy = orig.copy()
-hsv = cv2.cvtColor(origCopy, cv2.COLOR_BGR2HSV)
-# define range of blue color in HSV
-lower_blue = np.array([98,100,100])
-upper_blue = np.array([130,255,255])
-# lower_blue = np.array([0,100,100])
-# upper_blue = np.array([30,255,255])
-mask = cv2.inRange(hsv, lower_blue, upper_blue)
-mask = cv2.medianBlur(mask,7)
-
-# Here we need to check the version because the MSER object has changed between versions
-# You MAY need to install libgl1-mesa-dri
-
-version = cv2.__version__
-
-print("Using Open CV version: " + version)
-
-if int(version[0]) < 3:
-    detector = cv2.MSER()
-else:
-    detector = cv2.MSER_create()
-
-fs = detector.detect(mask)
-fs.sort(key = lambda x: -x.size)
-
-sfs = [x for x in fs if not supress(x)]
-
-circleMap = {"circleLocations" : [parseCircle(x) for x in sfs]}
-
-outputFile.write(json.dumps(circleMap, indent=4))
-
-
-# These were way too confusing being in the middle of the program....
-
 def parseCircle(circle):
     x = int(circle.pt[0])
     y = int(circle.pt[1])
@@ -62,3 +19,44 @@ def supress(x):
                 dist = math.sqrt(distx*distx + disty*disty)
                 if (f.size > x.size) and (dist<f.size/2):
                         return True
+                        
+# check if executing this script
+if __name__ == "__main__":
+    # parse input parameters
+    parser = argparse.ArgumentParser(description="Get speaker coordinates from map")
+    parser.add_argument('imagePath', metavar="I")
+    parser.add_argument('outputPath', metavar="O", type=argparse.FileType('w'))
+    parsedInput = vars(parser.parse_args())
+    outputFile = parsedInput["outputPath"]
+    inputFileName = parsedInput["imagePath"]
+    
+    orig = cv2.imread(inputFileName)
+    origCopy = orig.copy()
+    hsv = cv2.cvtColor(origCopy, cv2.COLOR_BGR2HSV)
+    # define range of blue color in HSV
+    lower_blue = np.array([98,100,100])
+    upper_blue = np.array([130,255,255])
+    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+    # get rid of the specks in the image
+    mask = cv2.medianBlur(mask,7)
+    
+    # Here we need to check the version because the MSER object has changed between versions
+    # You MAY need to install libgl1-mesa-dri
+    
+    version = cv2.__version__
+    
+    print("Using Open CV version: " + version)
+    
+    if int(version[0]) < 3:
+        detector = cv2.MSER()
+    else:
+        detector = cv2.MSER_create()
+    
+    fs = detector.detect(mask)
+    fs.sort(key = lambda x: -x.size)
+    
+    sfs = [x for x in fs if not supress(x)]
+    
+    circleMap = {"circleLocations" : [parseCircle(x) for x in sfs]}
+    
+    outputFile.write(json.dumps(circleMap, indent=4))
